73b706afe677518af66ae19dd7a27faa4ed86e84
조윤식 <cho.yunsig56@gmail.com>
2025-01-19 06:30:14 +0900

    :lipstick: 화면 구현 추가
    지도 화면의 일기 카드, 카테고리 선택 언더바 완성
    
    관련 이슈 번호: #5

--- feature/footprint/src/main/java/com/umc/footprint/FootprintApp.kt
+++ feature/footprint/src/main/java/com/umc/footprint/FootprintApp.kt
@@ -1,30 +1,44 @@
 package com.umc.footprint
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.ImageBitmap
+import java.time.LocalDate
 
 @Composable
 fun FootprintApp(
     viewModel: FootprintViewModel,
 ) {
     var isCategoryCardVisible by remember { mutableStateOf(false) }
+    var isDiaryCardFlipped by remember { mutableStateOf(false) }
+
+    LaunchedEffect(key1 = viewModel.clickedMarkerPosition) {
+        isDiaryCardFlipped = false
+    }
 
     FootprintScreen(
-        mapView = {
-            MapView(
-                onMapReady = { map -> viewModel.onMapReady(map) }
+        mapView = viewModel.getMapView(),
+        isCategoryMenuVisible = isCategoryCardVisible,
+        diaryCardProp = viewModel.clickedMarkerPosition?.let { (x, y) ->
+            DiaryCardProp(
+                x = x,
+                y = y,
+                date = LocalDate.now(),
+                image = ImageBitmap(width = 220, height = 220),
+                content = "This is diary.",
+                isFlipped = isDiaryCardFlipped,
+                onCardClicked = { isDiaryCardFlipped = !isDiaryCardFlipped },
+                onModifyButtonClicked = {},
             )
         },
-        categorySelectionCard = {
-            if (isCategoryCardVisible) CategorySelectionCard(
-                userName = "test",
-                props = listOf(),
-            )
-        },
+        userName = "test",
+        categoryItemProps = listOf(),
+        onNewCategoryButtonClicked = {},
         onCategoryButtonClicked = { isCategoryCardVisible = !isCategoryCardVisible },
-        onLocationButtonClicked = {}
+        onLocationButtonClicked = {},
     )
 }
--- feature/footprint/src/main/java/com/umc/footprint/FootprintViewModel.kt
+++ feature/footprint/src/main/java/com/umc/footprint/FootprintViewModel.kt
@@ -1,18 +1,37 @@
 package com.umc.footprint
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
-import com.naver.maps.map.NaverMap
+import androidx.lifecycle.viewModelScope
+import com.umc.footprint.core.MapHandler
+import com.umc.footprint.core.MapMarker
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class FootprintViewModel : ViewModel() {
-    private var map: NaverMap? = null
+@HiltViewModel
+class FootprintViewModel @Inject constructor(
+    private val mapHandler: MapHandler
+) : ViewModel() {
 
-    fun onMapReady(map: NaverMap) {
-        this.map = map
+    private val clickedMarkerPositionState = mutableStateOf<Pair<Float, Float>?>(null)
+
+    val clickedMarkerPosition get() = clickedMarkerPositionState.value
+
+    fun getMapView(): @Composable () -> Unit {
+        return mapHandler.getMapView()
     }
 
-    fun getViewingPosition(): Pair<Double, Double>? {
-        return map?.cameraPosition?.target?.let { target ->
-            target.latitude to target.longitude
-        }
+    fun markPositionOnMap(latitude: Double, longitude: Double) {
+        val marker = MapMarker(
+            latitude = latitude,
+            longitude = longitude,
+            onClicked = onClicked@ { x, y ->
+                clickedMarkerPositionState.value = x to y
+                return@onClicked { clickedMarkerPositionState.value = null }
+            }
+        )
+        viewModelScope.launch { mapHandler.addMarker(marker) }
     }
 }
