72263987703a7fb3e71405b45430fa26bb6ed50d
조윤식 <cho.yunsig56@gmail.com>
2025-01-29 11:14:30 +0900

    :heavy_plus_sign: 의존성 추가
    OkHttp 로깅, 테스트용 라이브러리 추가
    
    관련 이슈 번호: #13

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     testImplementation(libs.kotlinx.coroutines.test)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.kotlinx.coroutines.test)
 
     // 의존성 정의
     implementation(project(":core"))
@@ -77,6 +78,7 @@ dependencies {
     // Retrofit2
     implementation(libs.retrofit2)
     implementation(libs.retrofit2.converter.gson)
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
 
     // SharedPreference
     implementation(libs.preference.ktx)
