e964831d03f4150cb39cc1eaa0d7ab9fb398b816
deephoon <deephoony@gmail.com>
2025-01-31 18:41:48 +0900

    :sparkles: 역할 변경

--- feature/login/src/main/java/com/umc/login/MainNavigation.kt
+++ feature/login/src/main/java/com/umc/login/MainNavigation.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.umc.login.Join.JoinParentScreen
 
 @Composable
 fun MainNavigation(navController: NavHostController) {
