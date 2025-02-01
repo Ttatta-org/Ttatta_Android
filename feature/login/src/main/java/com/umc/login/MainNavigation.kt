35a83af2cec8a0a902e51601c573ef77da1c28fc
deephoon <deephoony@gmail.com>
2025-02-02 02:13:06 +0900

    :sparkles: 비밀번호,아이디 찾기 구현 후 연결

--- feature/login/src/main/java/com/umc/login/MainNavigation.kt
+++ feature/login/src/main/java/com/umc/login/MainNavigation.kt
@@ -4,6 +4,9 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.umc.login.FindId.FindIdScreen
+import com.umc.login.FindPw.FindPwScreen1
+import com.umc.login.FindPw.FindPwScreen2
 import com.umc.login.Join.JoinParentScreen
 
 @Composable
@@ -17,6 +20,15 @@ fun MainNavigation(navController: NavHostController) {
         }
         composable("join") {
             JoinParentScreen(navController)
+        }
+        composable("find_id") {
+            FindIdScreen(navController)
+        }
+        composable("find_pw") {
+            FindPwScreen1(navController)
+        }
+        composable("find_pw2") {
+            FindPwScreen2(navController)
         }
     }
 }
