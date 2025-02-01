56d282d6b7b19da626856784e2276b5562c0a98a
deephoon <deephoony@gmail.com>
2025-02-01 18:02:49 +0900

    :sparkles: feature 병합을 위해 버전 통일

--- feature/login/src/main/java/com/umc/login/LoginScreen.kt
+++ feature/login/src/main/java/com/umc/login/LoginScreen.kt
@@ -41,7 +41,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.test.isFocused
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
