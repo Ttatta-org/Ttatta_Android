6ec7d20ac2674d61bacee4babfdcea2329214b8f
조윤식 <cho.yunsig56@gmail.com>
2025-02-04 20:49:56 +0900

    :art: 다이얼로그 수정
    ModalBottomSheet를 사용한 코드로 수정
    
    관련 이슈 번호: #16

--- feature/category/src/main/java/com/umc/category/component/CategoryManagementBar.kt
+++ feature/category/src/main/java/com/umc/category/component/CategoryManagementBar.kt
@@ -1,22 +1,22 @@
 package com.umc.category.component
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -35,78 +35,68 @@ data class CategoryManagementBarProp(
 
 private val categoryManagementBarShape = RoundedCornerShape(topStart = 32.dp, topEnd = 32.dp)
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CategoryManagementBar(
     prop: CategoryManagementBarProp
 ) {
-    InsetProviderDialog(
-        onDismissed = prop.onDismissed
-    ) { inset ->
-        Box(
-            contentAlignment = Alignment.BottomCenter,
-            modifier = Modifier.fillMaxSize(),
-        ) {
+    ModalBottomSheet(
+        onDismissRequest = prop.onDismissed,
+        shape = categoryManagementBarShape,
+        containerColor = Color.White,
+        scrimColor = Color.Transparent,
+        dragHandle = {
             Box(
+                modifier = Modifier.padding(8.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = Res.drawable.ic_header_deco),
+                    contentDescription = null,
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier.size(32.dp)
+                )
+            }
+        },
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(space = 8.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 32.dp, end = 32.dp, top = 16.dp, bottom = 32.dp)
+        ) {
+            Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .shadow(
-                        shape = categoryManagementBarShape, elevation = 8.dp
-                    )
-                    .background(
-                        color = Color.White, shape = categoryManagementBarShape
-                    )
-                    .clickable(
-                        indication = null,
-                        interactionSource = null,
-                        onClick = {},
-                    )
+                    .clip(RoundedCornerShape(percent = 50))
+                    .clickable { prop.onModifyOptionClicked() }
             ) {
-                Column {
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.spacedBy(space = 16.dp),
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(8.dp)
-                    ) {
-                        Image(
-                            painter = painterResource(id = Res.drawable.ic_header_deco),
-                            contentDescription = null,
-                            contentScale = ContentScale.Fit,
-                            modifier = Modifier.size(32.dp)
-                        )
-                        Column(
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            verticalArrangement = Arrangement.spacedBy(space = 16.dp),
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(start = 32.dp, end = 32.dp, bottom = 32.dp)
-                        ) {
-                            Row(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .clickable { prop.onModifyOptionClicked() }
-                            ) {
-                                Text(text = stringResource(id = R.string.modify))
-                            }
-                            Row(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .clickable { prop.onDeleteCategoryOptionClicked() }
-                            ) {
-                                Text(text = stringResource(id = R.string.delete))
-                            }
-                            Row(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .clickable { prop.onDeleteCategoryAndAllIncludedDiariesOptionClicked() }
-                            ) {
-                                Text(text = stringResource(id = R.string.delete_all))
-                            }
-                        }
-                    }
-                    inset()
-                }
+                Text(
+                    text = stringResource(id = R.string.modify),
+                    modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+                )
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(percent = 50))
+                    .clickable { prop.onDeleteCategoryOptionClicked() }
+            ) {
+                Text(
+                    text = stringResource(id = R.string.delete),
+                    modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+                )
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(percent = 50))
+                    .clickable { prop.onDeleteCategoryAndAllIncludedDiariesOptionClicked() }
+            ) {
+                Text(
+                    text = stringResource(id = R.string.delete_all),
+                    modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+                )
             }
         }
     }
