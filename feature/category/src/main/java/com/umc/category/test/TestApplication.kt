239e24901eecd7e049f871315fdb7f353854b623
조윤식 <cho.yunsig56@gmail.com>
2025-02-07 16:12:35 +0900

    :boom: 테스트 모듈 추가
    테스트 코드를 작성할 수 있는 모듈을 추가
    
    관련 이슈 번호: #32

--- feature/category/src/main/java/com/umc/category/test/TestApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.umc.category.test
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class TestApplication: Application()
---
+++ test/category/src/main/java/com/umc/ttatta/test/category/TestApplication.kt
@@ -0,0 +1,7 @@
+package com.umc.ttatta.test.category
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TestApplication: Application()
