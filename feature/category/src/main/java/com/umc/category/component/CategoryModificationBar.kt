6d19824a8422a7f021f98ecf0382de6f4b8c08c8
조윤식 <cho.yunsig56@gmail.com>
2025-02-14 18:28:51 +0900

    :lipstick: 바텀바 요소 그림자 추가
    그림자가 표시되도록 수정
    
    관련 이슈 번호: #16

--- feature/category/src/main/java/com/umc/category/component/CategoryModificationBar.kt
+++ feature/category/src/main/java/com/umc/category/component/CategoryModificationBar.kt
@@ -9,10 +9,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
@@ -24,7 +28,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
@@ -63,177 +69,193 @@ fun CategoryModificationBar(
     prop: CategoryModificationBarProp
 ) {
     val density = LocalDensity.current
+    val bottom = WindowInsets.systemBars.asPaddingValues().calculateBottomPadding()
 
     ModalBottomSheet(
         onDismissRequest = prop.onDismissed,
-        shape = categoryModificationBarShape,
-        containerColor = Color.White,
+        shape = RectangleShape,
+        sheetMaxWidth = 1024.dp,
+        containerColor = Color.Transparent,
         scrimColor = Color.Transparent,
-        dragHandle = {
-            Box(
-                modifier = Modifier.padding(8.dp)
-            ) {
-                Image(
-                    painter = painterResource(id = Res.drawable.ic_header_deco),
-                    contentDescription = null,
-                    contentScale = ContentScale.Fit,
-                    modifier = Modifier.size(32.dp)
-                )
-            }
-        },
+        dragHandle = {},
+        contentWindowInsets = { WindowInsets(bottom = 0.dp) },
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(space = 48.dp),
-            modifier = Modifier.padding(
-                start = 32.dp,
-                end = 32.dp,
-                top = 16.dp,
-                bottom = 32.dp
-            )
-        ) {
+        Column {
+            // 그림자를 위한 여백
+            Spacer(modifier = Modifier.height(16.dp))
+            // 본문
             Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(space = 16.dp)
+                modifier = Modifier
+                    .shadow(
+                        elevation = 16.dp,
+                        shape = categoryModificationBarShape,
+                    )
+                    .background(
+                        color = Color.White,
+                        shape = categoryModificationBarShape,
+                    )
             ) {
-                Text(
-                    text = stringResource(id = R.string.modify),
-                    fontWeight = FontWeight.Bold,
-                )
                 Column(
-                    verticalArrangement = Arrangement.spacedBy(space = 24.dp)
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.padding(start = 32.dp, end = 32.dp, bottom = 32.dp)
                 ) {
-                    // 이름 입력 창
+                    Box(
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Image(
+                            painter = painterResource(id = Res.drawable.ic_header_deco),
+                            contentDescription = null,
+                            contentScale = ContentScale.Fit,
+                            modifier = Modifier.size(32.dp)
+                        )
+                    }
                     Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
                         verticalArrangement = Arrangement.spacedBy(space = 16.dp)
                     ) {
                         Text(
-                            text = stringResource(id = R.string.modify_footprint),
+                            text = stringResource(id = R.string.modify),
                             fontWeight = FontWeight.Bold,
-                            color = Color.Primary300,
-                            modifier = Modifier.padding(horizontal = 16.dp)
                         )
-                        BasicTextField(
-                            value = prop.categoryNameInputFieldValue,
-                            onValueChange = prop.onCategoryNameInputFieldValueChanged,
-                            textStyle = TextStyle(
-                                fontSize = 12.sp
-                            )
-                        ) { innerTextField ->
-                            Box(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .border(
-                                        width = 1.dp,
-                                        color = Color.Primary500,
-                                        shape = RoundedCornerShape(percent = 50)
+                        Column(
+                            verticalArrangement = Arrangement.spacedBy(space = 24.dp)
+                        ) {
+                            // 이름 입력 창
+                            Column(
+                                verticalArrangement = Arrangement.spacedBy(space = 16.dp)
+                            ) {
+                                Text(
+                                    text = stringResource(id = R.string.modify_footprint),
+                                    fontWeight = FontWeight.Bold,
+                                    color = Color.Primary300,
+                                    modifier = Modifier.padding(horizontal = 16.dp)
+                                )
+                                BasicTextField(
+                                    value = prop.categoryNameInputFieldValue,
+                                    onValueChange = prop.onCategoryNameInputFieldValueChanged,
+                                    textStyle = TextStyle(
+                                        fontSize = 12.sp
                                     )
-                                    .background(
-                                        color = Color.White,
-                                        shape = RoundedCornerShape(percent = 50)
-                                    )
-                                    .padding(vertical = 16.dp, horizontal = 24.dp)
+                                ) { innerTextField ->
+                                    Box(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .border(
+                                                width = 1.dp,
+                                                color = Color.Primary500,
+                                                shape = RoundedCornerShape(percent = 50)
+                                            )
+                                            .background(
+                                                color = Color.White,
+                                                shape = RoundedCornerShape(percent = 50)
+                                            )
+                                            .padding(vertical = 16.dp, horizontal = 24.dp)
+                                    ) {
+                                        Row(
+                                            horizontalArrangement = Arrangement.SpaceBetween,
+                                            verticalAlignment = Alignment.CenterVertically,
+                                            modifier = Modifier.fillMaxWidth()
+                                        ) {
+                                            Box(
+                                                contentAlignment = Alignment.CenterStart,
+                                                modifier = Modifier
+                                                    .weight(1f)
+                                                    .height(with(density) { 16.sp.toDp() })
+                                            ) {
+                                                if (prop.categoryNameInputFieldValue.isBlank()) Text(
+                                                    text = stringResource(id = R.string.footprint_placeholder),
+                                                    fontSize = 12.sp,
+                                                    color = Color.Grey300,
+                                                )
+                                                innerTextField()
+                                            }
+                                            Text(
+                                                text = buildAnnotatedString {
+                                                    append(prop.categoryNameInputFieldValue.length.toString())
+                                                    withStyle(
+                                                        style = SpanStyle(color = Color.Grey300)
+                                                    ) {
+                                                        append("/${prop.maxCategoryNameLength}")
+                                                    }
+                                                },
+                                                fontSize = 12.sp,
+                                            )
+                                        }
+                                    }
+                                }
+                            }
+                            // 색상 선택 창
+                            Column(
+                                verticalArrangement = Arrangement.spacedBy(space = 8.dp)
                             ) {
                                 Row(
-                                    horizontalArrangement = Arrangement.SpaceBetween,
-                                    verticalAlignment = Alignment.CenterVertically,
-                                    modifier = Modifier.fillMaxWidth()
-                                ) {
-                                    Box(
-                                        contentAlignment = Alignment.CenterStart,
-                                        modifier = Modifier
-                                            .weight(1f)
-                                            .height(with(density) { 16.sp.toDp() })
-                                    ) {
-                                        if (prop.categoryNameInputFieldValue.isBlank()) Text(
-                                            text = stringResource(id = R.string.footprint_placeholder),
-                                            fontSize = 12.sp,
-                                            color = Color.Grey300,
-                                        )
-                                        innerTextField()
-                                    }
-                                    Text(
-                                        text = buildAnnotatedString {
-                                            append(prop.categoryNameInputFieldValue.length.toString())
-                                            withStyle(
-                                                style = SpanStyle(color = Color.Grey300)
-                                            ) {
-                                                append("/${prop.maxCategoryNameLength}")
-                                            }
-                                        },
-                                        fontSize = 12.sp,
-                                    )
-                                }
-                            }
-                        }
-                    }
-                    // 색상 선택 창
-                    Column(
-                        verticalArrangement = Arrangement.spacedBy(space = 8.dp)
-                    ) {
-                        Row(
-                            horizontalArrangement = Arrangement.spacedBy(space = 8.dp),
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-                            Image(
-                                painter = painterResource(id = com.umc.design.R.drawable.ic_header_deco),
-                                contentDescription = null,
-                                contentScale = ContentScale.Fit,
-                                modifier = Modifier.size(32.dp)
-                            )
-                            Text(
-                                text = stringResource(id = R.string.color_choice),
-                                fontWeight = FontWeight.Bold,
-                                color = Color.Primary300
-                            )
-                        }
-                        Row(
-                            horizontalArrangement = Arrangement.spacedBy(space = 12.dp),
-                            modifier = Modifier
-                                .horizontalScroll(state = rememberScrollState())
-                        ) {
-                            CategoryColor.entries.forEach { color ->
-                                Box(
-                                    contentAlignment = Alignment.Center,
-                                    modifier = Modifier.clickable {
-                                        prop.onCategoryColorClicked(
-                                            color
-                                        )
-                                    }
+                                    horizontalArrangement = Arrangement.spacedBy(space = 8.dp),
+                                    verticalAlignment = Alignment.CenterVertically
                                 ) {
                                     Image(
-                                        painter = painterResource(id = color.flowerIconId),
+                                        painter = painterResource(id = com.umc.design.R.drawable.ic_header_deco),
                                         contentDescription = null,
                                         contentScale = ContentScale.Fit,
-                                        modifier = Modifier.size(28.dp)
+                                        modifier = Modifier.size(32.dp)
                                     )
-                                    if (color == prop.selectedColor) Image(
-                                        painter = painterResource(id = R.drawable.ic_check),
-                                        contentDescription = null,
-                                        contentScale = ContentScale.Fit,
-                                        modifier = Modifier.size(12.dp)
+                                    Text(
+                                        text = stringResource(id = R.string.color_choice),
+                                        fontWeight = FontWeight.Bold,
+                                        color = Color.Primary300
                                     )
+                                }
+                                Row(
+                                    horizontalArrangement = Arrangement.spacedBy(space = 12.dp),
+                                    modifier = Modifier
+                                        .horizontalScroll(state = rememberScrollState())
+                                ) {
+                                    CategoryColor.entries.forEach { color ->
+                                        Box(
+                                            contentAlignment = Alignment.Center,
+                                            modifier = Modifier.clickable {
+                                                prop.onCategoryColorClicked(
+                                                    color
+                                                )
+                                            }
+                                        ) {
+                                            Image(
+                                                painter = painterResource(id = color.flowerIconId),
+                                                contentDescription = null,
+                                                contentScale = ContentScale.Fit,
+                                                modifier = Modifier.size(28.dp)
+                                            )
+                                            if (color == prop.selectedColor) Image(
+                                                painter = painterResource(id = R.drawable.ic_check),
+                                                contentDescription = null,
+                                                contentScale = ContentScale.Fit,
+                                                modifier = Modifier.size(12.dp)
+                                            )
+                                        }
+                                    }
                                 }
                             }
                         }
                     }
+                    ElevatedButton(
+                        shape = RoundedCornerShape(percent = 50),
+                        onClick = prop.onDoneButtonClicked,
+                        colors = ButtonDefaults.elevatedButtonColors(
+                            containerColor = Color.Primary200,
+                        ),
+                        modifier = Modifier
+                            .fillMaxWidth(0.5f)
+                            .padding(top = 48.dp),
+                        elevation = ButtonDefaults.elevatedButtonElevation(
+                            defaultElevation = 4.dp
+                        )
+                    ) {
+                        Text(
+                            text = stringResource(id = R.string.done),
+                            color = Color.White
+                        )
+                    }
                 }
-            }
-            ElevatedButton(
-                shape = RoundedCornerShape(percent = 50),
-                onClick = prop.onDoneButtonClicked,
-                colors = ButtonDefaults.elevatedButtonColors(
-                    containerColor = Color.Primary200,
-                ),
-                modifier = Modifier.fillMaxWidth(0.5f),
-                elevation = ButtonDefaults.elevatedButtonElevation(
-                    defaultElevation = 4.dp
-                )
-            ) {
-                Text(
-                    text = stringResource(id = R.string.done),
-                    color = Color.White
-                )
+                Spacer(modifier = Modifier.height(bottom))
             }
         }
     }
