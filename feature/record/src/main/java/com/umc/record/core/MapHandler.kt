96395799900c56ccf410e837823a0635607494df
조윤식 <cho.yunsig56@gmail.com>
2025-02-16 22:51:10 +0900

    :lipstick: MapHandler 구현
    지도 화면 핸들러 구현
    
    관련 이슈 번호: #4

--- feature/record/src/main/java/com/umc/record/core/MapHandler.kt
+++ feature/record/src/main/java/com/umc/record/core/MapHandler.kt
@@ -2,35 +2,13 @@ package com.umc.record.core
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
-import com.umc.design.CategoryColor
 
 val locatorWidth = 48.dp
 val locatorHeight = 48.dp
-val clusteredMarkerMaxWidth = 128.dp
-val clusteredMarkerMaxHeight = 128.dp
-val markerWidth = 64.dp
-val markerHeight = 64.dp
-val clusteringDp = 32.dp
-
-data class MapMarker(
-    val latitude: Double,
-    val longitude: Double,
-    val zIndex: Int,
-    val color: CategoryColor? = null,
-    val onClicked: ((Float, Float) -> (() -> Unit)?)? = null,
-)
 
 interface MapHandler {
-    @Composable fun MapView(
-        isBlurApplied: Boolean,
-        isLocationMarkingEnabled: Boolean,
-    )
+    @Composable fun MapView(isLocationMarkingEnabled: Boolean)
 
-    suspend fun moveTo(latitude: Double, longitude: Double)
-    suspend fun moveToCurrentPosition()
-    suspend fun getViewingPosition(): Pair<Double, Double>?
-    suspend fun addMarker(marker: MapMarker)
-    suspend fun removeMarker(marker: MapMarker)
-    suspend fun getAllMarkers(): List<MapMarker>
-    suspend fun removeAllMarkers()
+    suspend fun getCurrentPinnedCoordination(): Pair<Double, Double>  // (위도, 경도)
+    suspend fun movePin(latitude: Double, longitude: Double)
 }
