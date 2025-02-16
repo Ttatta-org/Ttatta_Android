48b948c2c5016a8ac9a1003877a9a764ae2d4e7e
조윤식 <cho.yunsig56@gmail.com>
2025-02-16 18:29:13 +0900

    :lipstick: 디자이너 요구사항 반영
    클러스터링 마커 표시 확대 수준 반영
    
    관련 이슈 번호: #5

--- feature/footprint/src/main/java/com/umc/footprint/data/LocationHandlerImpl.kt
+++ feature/footprint/src/main/java/com/umc/footprint/data/LocationHandlerImpl.kt
@@ -7,6 +7,9 @@ import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.Priority
 import com.umc.footprint.core.LocationHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
@@ -14,8 +17,21 @@ class LocationHandlerImpl @Inject constructor(
     private val locationSource: FusedLocationProviderClient,
 ) : LocationHandler {
 
+    private var isCalculatingLocation = false
+
     @SuppressLint("MissingPermission")
     override suspend fun getCurrentLocation(): Location {
+        if (!isCalculatingLocation) {
+            isCalculatingLocation = true
+            CoroutineScope(Dispatchers.IO).launch {
+                locationSource.getCurrentLocation(
+                    Priority.PRIORITY_BALANCED_POWER_ACCURACY,
+                    null
+                ).addOnCompleteListener {
+                    isCalculatingLocation = false
+                }
+            }
+        }
         return locationSource.lastLocation.await()
     }
 
