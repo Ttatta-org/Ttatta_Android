4cbde16761c466178f5c6b4c5beffcec4c4ed514
조윤식 <cho.yunsig56@gmail.com>
2025-02-18 01:44:57 +0900

    :lipstick: TopBar 요소 추가
    탑 바 구현
    
    관련 이슈 번호: #36

---
+++ feature/challenge/src/main/java/com/umc/challenge/component/PointChip.kt
@@ -0,0 +1,115 @@
+package com.umc.challenge.component
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.umc.challenge.R
+import com.umc.design.Primary200
+import com.umc.design.Primary300
+import com.umc.design.Secondary100
+import com.umc.design.Secondary300
+
+enum class PointChipTheme(
+    val backgroundColor: Color,
+    @DrawableRes val icon: Int
+) {
+    GOLD(
+        backgroundColor = Color.Secondary300,
+        icon = R.drawable.ic_point1
+    ),
+    EAGLE(
+        backgroundColor = Color.Primary200,
+        icon = R.drawable.ic_point2
+    ),
+}
+
+@Composable
+fun PointChip(
+    point: Int,
+    theme: PointChipTheme = PointChipTheme.GOLD
+) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .clip(RoundedCornerShape(percent = 50))
+            .background(color = theme.backgroundColor)
+            .height(24.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(2.dp),
+            modifier = Modifier.padding(2.dp)
+        ) {
+            Image(
+                painter = painterResource(id = theme.icon),
+                contentDescription = null,
+                contentScale = ContentScale.Fit,
+                modifier = Modifier.fillMaxHeight()
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .width(48.dp)
+                    .clip(RoundedCornerShape(percent = 50))
+                    .background(color = Color.Secondary100)
+            ) {
+                Box(
+                    contentAlignment = Alignment.CenterEnd,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 4.dp, vertical = 2.dp)
+                ) {
+                    Text(
+                        text = point.toString().replace(
+                            regex = Regex("\\B(?=(\\d{3})+(?!\\d))"),
+                            replacement = ","
+                        ),
+                        color = Color.Primary300,
+                        fontSize = 12.sp,
+                        fontWeight = FontWeight.Bold,
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewGoldPointChip() {
+    PointChip(
+        point = 1300,
+        theme = PointChipTheme.GOLD
+    )
+}
+
+@Preview
+@Composable
+fun PreviewEaglePointChip2() {
+    PointChip(
+        point = 1300,
+        theme = PointChipTheme.EAGLE
+    )
+}
---
+++ feature/challenge/src/main/java/com/umc/challenge/component/TitledTopBar.kt
@@ -0,0 +1,155 @@
+package com.umc.challenge.component
+
+import android.graphics.BitmapFactory
+import androidx.annotation.RawRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil3.compose.rememberAsyncImagePainter
+import coil3.request.ImageRequest
+import coil3.svg.SvgDecoder
+import com.umc.challenge.R
+import com.umc.design.Primary200
+import com.umc.design.Secondary300
+
+private const val topBarResourceWidthRatio = 390f
+private const val topBarResourceCroppedHeightRatio = 87f
+
+enum class TitledTopBarMode(
+    val backgroundColor: Color,
+    @RawRes val backgroundImage: Int,
+    @RawRes val backgroundPreviewImage: Int,
+    val title: String,
+) {
+    SHOP(
+        backgroundColor = Color.Primary200,
+        backgroundImage = R.raw.img_top_bar_shop,
+        backgroundPreviewImage = R.raw.img_top_bar_shop_for_preview,
+        title = "SHOP"
+    ),
+    MY_ITEM(
+        backgroundColor = Color.Secondary300,
+        backgroundImage = R.raw.img_top_bar_my_item,
+        backgroundPreviewImage = R.raw.img_top_bar_my_item_for_preview,
+        title = "MY ITEM"
+    )
+}
+
+@Composable
+fun TitledTopBar(
+    mode: TitledTopBarMode
+) {
+    val context = LocalContext.current
+    val density = LocalDensity.current
+    val statusBarHeight = WindowInsets.statusBars.asPaddingValues().calculateTopPadding()
+    var screenWidth by remember { mutableStateOf(0.dp) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .onGloballyPositioned { with(density) { screenWidth = it.size.width.toDp() } }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = mode.backgroundColor)
+        ) {
+            Spacer(modifier = Modifier.height(statusBarHeight))
+            Box(
+                contentAlignment = Alignment.BottomCenter,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(4.dp)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 24.dp, vertical = 8.dp)
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_logo),
+                        contentDescription = null,
+                        tint = Color.White,
+                        modifier = Modifier.size(32.dp)
+                    )
+                }
+                Text(
+                    text = mode.title,
+                    color = Color.White,
+                    fontSize = with(density) { 16.dp.toSp() }
+                )
+            }
+        }
+        Image(
+            painter = rememberAsyncImagePainter(
+                model = ImageRequest.Builder(context)
+                    .data("android.resource://${context.packageName}/${mode.backgroundImage}")
+                    .decoderFactory(SvgDecoder.Factory()).build(),
+                // 프리뷰를 위한 이미지
+                error = BitmapPainter(
+                    image = BitmapFactory.decodeResource(
+                        context.resources, mode.backgroundPreviewImage
+                    ).asImageBitmap(),
+                )
+            ),
+            contentScale = ContentScale.FillWidth,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .clipToBounds()
+                .offset {
+                    IntOffset(
+                        x = 0,
+                        y = -screenWidth
+                            .times(topBarResourceCroppedHeightRatio)
+                            .div(topBarResourceWidthRatio)
+                            .roundToPx(),
+                    )
+                }
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewShopTitledTopBar() {
+    TitledTopBar(mode = TitledTopBarMode.SHOP)
+}
+
+@Preview
+@Composable
+fun PreviewMyItemTitledTopBar() {
+    TitledTopBar(mode = TitledTopBarMode.MY_ITEM)
+}
