b61933127e6b8f3a2015e268f54e6e88b7ace0d8
조윤식 <cho.yunsig56@gmail.com>
2025-02-18 01:45:40 +0900

    :sparkles: 초기 구현
    ChallengeApp 초기 로직 구현
    
    관련 이슈 번호: #36

--- feature/challenge/src/main/java/com/umc/challenge/ChallengeApp.kt
+++ feature/challenge/src/main/java/com/umc/challenge/ChallengeApp.kt
@@ -3,6 +3,14 @@ package com.umc.challenge
 import androidx.compose.runtime.Composable
 
 @Composable
-fun ChallengeApp(viewModel: ChallengeViewModel) {
-    ChallengeScreen()
+fun ChallengeApp(
+    viewModel: ChallengeViewModel,
+    onNavigationBarVisibilityChanged: (Boolean) -> Unit,
+) {
+    ChallengeScreen(
+        topBarProp = previewChallengeScreenTopBarProp,
+        challengeItemPropList = previewChallengeItemPropList,
+        accessorySet = previewAccessorySet,
+        onNewChallengeButtonClicked = {}
+    )
 }
