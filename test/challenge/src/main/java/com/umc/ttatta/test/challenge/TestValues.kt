39adf2f99172eb3efa8f075496cb117a1592d8e6
조윤식 <cho.yunsig56@gmail.com>
2025-02-18 22:29:05 +0900

    :white_check_mark: 화면 테스트 작성
    테스트 작성
    
    관련 이슈 번호: #36

---
+++ test/challenge/src/main/java/com/umc/ttatta/test/challenge/TestValues.kt
@@ -0,0 +1,9 @@
+package com.umc.ttatta.test.challenge
+
+object TestValues {
+    const val ID = "challenge_test"
+    const val PASSWORD = "test1234"
+    const val NICKNAME = "tester"
+    const val NAME = "김땡땡"
+    const val EMAIL = "test@naver.com"
+}
