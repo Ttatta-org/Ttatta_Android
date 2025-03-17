03b7ca2b7c4feddca088dd1fe33f1809f3802d51
조윤식 <cho.yunsig56@gmail.com>
2025-03-17 13:30:36 +0900

    :white_check_mark: 테스트 업데이트
    테스트 값 수정
    
    관련 이슈 번호: #13

--- data/src/androidTest/java/com/umc/data/TestValue.kt
+++ data/src/androidTest/java/com/umc/data/TestValue.kt
@@ -4,7 +4,7 @@ import com.umc.design.CategoryColor
 import java.time.LocalDateTime
 
 object TestValue {
-    const val ID = "test_id_100"
+    const val ID = "test_id_101"
     const val PASSWORD = "test1234"
     const val NAME = "kim"
     const val NICKNAME = "tester"
