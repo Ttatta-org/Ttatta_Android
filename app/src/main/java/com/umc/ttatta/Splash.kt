8046caad4dda9af1c95107e661b2567e425c4d5f
조윤식 <cho.yunsig56@gmail.com>
2025-03-23 10:59:57 +0900

    :art: 스플래시 화면 리팩토링
    스플래시 리팩토링
    
    관련 이슈 번호: 없음

--- app/src/main/java/com/umc/ttatta/Splash.kt
+++ app/src/main/java/com/umc/ttatta/Splash.kt
@@ -9,19 +9,15 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -31,23 +27,24 @@ fun Splash() {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.White),
+            .background(Color(0xFFFFEEDE)),
         contentAlignment = Alignment.Center
     ) {
         Image(
             painter = painterResource(id = R.drawable.img_gradient_background),
             contentDescription = "배경 이미지",
-            contentScale = ContentScale.Fit // 이미지를 전체 영역에 맞게 확대/축소
+            contentScale = ContentScale.Fit, // 이미지를 전체 영역에 맞게 확대/축소
+            modifier = Modifier.fillMaxSize(),
         )
-        Column(modifier = Modifier
-            .fillMaxWidth(),
+        Column(
+            modifier = Modifier.fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+            verticalArrangement = Arrangement.spacedBy(space = 13.dp)
         ) {
             Image(
                 painter = painterResource(id = R.drawable.ic_bigttatta_logo),
                 contentDescription = "따따로고",
-                modifier = Modifier.size(149.dp, 130.dp),
+                modifier = Modifier.fillMaxWidth(0.5f),
                 contentScale = ContentScale.None // 이미지를 전체 영역에 맞게 확대/축소
             )
             Spacer(modifier = Modifier.height(13.81.dp))
@@ -55,13 +52,10 @@ fun Splash() {
                 text = "일상을 기록하다, 나만의 비밀공간",
                 style = TextStyle(
                     fontSize = 14.sp,
-                    fontWeight = FontWeight(600),
+                    fontWeight = FontWeight.W600,
                     color = Color(0xFFFF8072),
-
-                    textAlign = TextAlign.Center,
-                )
+                ),
             )
-
         }
     }
 }
