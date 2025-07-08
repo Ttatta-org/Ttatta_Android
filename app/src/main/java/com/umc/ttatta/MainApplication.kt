93e026639f3608c2f1ede6ec9d7fec3042c9a382
CHOYUNSIG <cho.yunsig56@gmail.com>
2025-07-08 14:18:25 +0900

    :loud_sound: 해시키를 보기 위한 로그 추가

--- app/src/main/java/com/umc/ttatta/MainApplication.kt
+++ app/src/main/java/com/umc/ttatta/MainApplication.kt
@@ -1,6 +1,7 @@
 package com.umc.ttatta
 
 import android.app.Application
+import android.util.Log
 import com.kakao.sdk.common.KakaoSdk
 import com.naver.maps.map.NaverMapSdk
 import dagger.hilt.android.HiltAndroidApp
@@ -14,5 +15,6 @@ class MainApplication: Application() {
             NaverMapSdk.NaverCloudPlatformClient(BuildConfig.NAVER_SDK_CLIENT_ID)
 
         KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
+        Log.d("MainApplication", "Kakao SDK hash Key: ${KakaoSdk.keyHash}")
     }
 }
