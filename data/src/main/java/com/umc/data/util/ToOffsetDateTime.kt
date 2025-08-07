c6b5d1a085a486794609b39deab5b1069282badf
CHOYUNSIG <cho.yunsig56@gmail.com>
2025-08-08 00:52:02 +0900

    :recycle: 반환값 명시

--- data/src/main/java/com/umc/data/util/ToOffsetDateTime.kt
+++ data/src/main/java/com/umc/data/util/ToOffsetDateTime.kt
@@ -2,6 +2,7 @@ package com.umc.data.util
 
 import java.time.Instant
 import java.time.LocalDateTime
+import java.time.OffsetDateTime
 import java.time.ZoneId
 
-fun LocalDateTime.toOffsetDateTimeInKorea() = atOffset(ZoneId.of("Asia/Seoul").rules.getOffset(Instant.now()))
+fun LocalDateTime.toOffsetDateTimeInKorea(): OffsetDateTime = atOffset(ZoneId.of("Asia/Seoul").rules.getOffset(Instant.now()))
