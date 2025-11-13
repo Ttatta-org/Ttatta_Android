c6ec8392551b028c838a58417a583e8648abe045
CHOYUNSIG <cho.yunsig56@gmail.com>
2025-11-13 10:23:16 +0900

    :zap: IO 코루틴으로 강제

--- core/src/main/java/com/umc/core/util/runWithScope.kt
+++ core/src/main/java/com/umc/core/util/runWithScope.kt
@@ -2,6 +2,9 @@ package com.umc.core.util
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-fun <VM: ViewModel, T> VM.runWithScope(block: suspend VM.() -> T) = this.viewModelScope.launch { block() }
+fun <VM : ViewModel, T> VM.runWithScope(block: suspend VM.() -> T) {
+    this.viewModelScope.launch(Dispatchers.IO) { block() }
+}
