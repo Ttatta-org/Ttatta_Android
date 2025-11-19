d177fab94984c898d9f06cb24c4418da2f6daf15
CHOYUNSIG <cho.yunsig56@gmail.com>
2025-11-19 21:40:43 +0900

    :bug: 로그아웃 시 핀 설정이 남아있던 버그 수정

--- data/src/main/java/com/umc/data/di/repository/UserRepositoryModule.kt
+++ data/src/main/java/com/umc/data/di/repository/UserRepositoryModule.kt
@@ -4,6 +4,7 @@ import com.umc.core.repository.UserRepository
 import com.umc.data.api.ServerApi
 import com.umc.data.implementation.repository.UserRepositoryImpl
 import com.umc.data.preference.AuthPreference
+import com.umc.data.preference.SettingPreference
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -17,11 +18,13 @@ object UserRepositoryModule {
     @Singleton
     fun provideUserRepository(
         serverApi: ServerApi,
-        authPreference: AuthPreference
+        authPreference: AuthPreference,
+        settingPreference: SettingPreference,
     ): UserRepository {
         return UserRepositoryImpl(
             serverApi = serverApi,
             authPreference = authPreference,
+            settingPreference = settingPreference,
         )
     }
 }
