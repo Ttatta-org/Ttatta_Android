5fb0a92f256004eb41fde6b49e5b5a050840ab8a
CHOYUNSIG <cho.yunsig56@gmail.com>
2025-11-20 02:21:05 +0900

    :bug: 토큰 리프레싱 동시성 문제 수정

--- data/src/main/java/com/umc/data/util/withAuth.kt
+++ data/src/main/java/com/umc/data/util/withAuth.kt
@@ -3,6 +3,11 @@ package com.umc.data.util
 import com.umc.data.api.ServerApi
 import com.umc.data.api.dto.BaseResponse
 import com.umc.data.preference.AuthPreference
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import retrofit2.HttpException
+
+private val mutex = Mutex(locked = false)
 
 suspend fun <T> ServerApi.withAuth(
     authPreference: AuthPreference,
@@ -10,10 +15,21 @@ suspend fun <T> ServerApi.withAuth(
 ): T {
     try {
         return withCheck { routine() }
-    } catch (_: Exception) {
-        val response = withCheck { refreshToken(authPreference.refreshToken!!) }
-        authPreference.refreshToken = response.refreshToken!!
-        authPreference.accessToken = response.accessToken!!
+    } catch (e: HttpException) {
+        if (e.code() != 200) throw e  // 토큰 만료 시의 백엔드의 응답 코드가 200임
+
+        val accessToken = e
+            .response()
+            ?.raw()?.request?.header("Authorization")
+
+        mutex.withLock {
+            if (authPreference.accessToken != null && accessToken != authPreference.accessToken) return@withLock
+
+            val response = withCheck { refreshToken(authPreference.refreshToken!!) }
+            authPreference.refreshToken = response.refreshToken!!
+            authPreference.accessToken = response.accessToken!!
+        }
+
         return withCheck { routine() }
     }
 }
