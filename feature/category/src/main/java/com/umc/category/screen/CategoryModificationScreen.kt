c3f769b26b7732a6e00591f5d8a11058d72e3d3c
CHOYUNSIG <cho.yunsig56@gmail.com>
2025-11-26 22:45:31 +0900

    :bug: 버튼이 내비게이션 바 아래에 가려지는 버그 수정

--- feature/category/src/main/java/com/umc/category/screen/CategoryModificationScreen.kt
+++ feature/category/src/main/java/com/umc/category/screen/CategoryModificationScreen.kt
@@ -10,9 +10,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
@@ -268,7 +271,13 @@ fun CategoryModificationScreen(
                     // 완료 버튼
                     Row(
                         horizontalArrangement = Arrangement.Center,
-                        modifier = Modifier.fillMaxWidth(),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(
+                                bottom = WindowInsets.navigationBars
+                                    .asPaddingValues()
+                                    .calculateBottomPadding()
+                            ),
                     ) {
                         CustomButton(
                             text = stringResource(id = R.string.done),
