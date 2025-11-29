f37de814405ddec776a4c25962efdf575d65850e
CHOYUNSIG <cho.yunsig56@gmail.com>
2025-11-30 02:54:06 +0900

    :lipstick: 아이템 상점 화면 변경사항 적용

--- feature/challenge/src/main/java/com/umc/challenge/component/PointChip.kt
+++ feature/challenge/src/main/java/com/umc/challenge/component/PointChip.kt
@@ -7,10 +7,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -25,22 +24,20 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.umc.challenge.R
-import com.umc.design.Primary200
-import com.umc.design.Primary300
-import com.umc.design.Secondary100
-import com.umc.design.Secondary300
+import com.umc.design.theme.LocalColorTheme
+import com.umc.design.theme.ThemeProvider
 
 enum class PointChipTheme(
-    val backgroundColor: Color,
-    @DrawableRes val icon: Int
+    val backgroundColor: @Composable () -> Color,
+    @get:DrawableRes val icon: Int
 ) {
     GOLD(
-        backgroundColor = Color.Secondary300,
-        icon = R.drawable.ic_point1
+        backgroundColor = { LocalColorTheme.current.primary[200] },
+        icon = R.drawable.img_point_icon_1
     ),
     EAGLE(
-        backgroundColor = Color.Primary200,
-        icon = R.drawable.ic_point2
+        backgroundColor = { LocalColorTheme.current.primary[400] },
+        icon = R.drawable.img_point_icon_2
     ),
 }
 
@@ -54,13 +51,13 @@ fun PointChip(
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
             .clip(RoundedCornerShape(percent = 50))
-            .background(color = theme.backgroundColor)
+            .background(color = theme.backgroundColor.invoke())
             .height(30.dp)
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(2.dp),
-            modifier = Modifier.padding(4.dp)
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
+            modifier = Modifier.padding(vertical = 4.dp, horizontal = 4.5.dp),
         ) {
             Image(
                 painter = painterResource(id = theme.icon),
@@ -69,28 +66,26 @@ fun PointChip(
                 modifier = Modifier.fillMaxHeight()
             )
             Box(
+                contentAlignment = Alignment.Center,
                 modifier = Modifier
                     .fillMaxHeight()
-                    .width(47.dp)
+                    .widthIn(min = 47.dp)
                     .clip(RoundedCornerShape(percent = 50))
-                    .background(color = Color.Secondary100)
+                    .background(color = LocalColorTheme.current.primary[100])
             ) {
-                Box(
-                    contentAlignment = Alignment.Center,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(horizontal = 6.dp, vertical = 4.dp)
-                ) {
-                    Text(
-                        text = point.toString().replace(
+                Text(
+                    text = point
+                        .toString()
+                        .replace(
                             regex = Regex("\\B(?=(\\d{3})+(?!\\d))"),
                             replacement = ","
                         ),
-                        color = Color.Primary300,
-                        fontSize = 13.sp,
-                        fontWeight = FontWeight.ExtraBold,
-                    )
-                }
+                    color = LocalColorTheme.current.primary[500],
+                    fontSize = 13.sp,
+                    fontWeight = FontWeight.ExtraBold,
+                    letterSpacing = (-0.4).sp,
+                    modifier = Modifier.padding(horizontal = 6.dp)
+                )
             }
         }
     }
@@ -99,17 +94,21 @@ fun PointChip(
 @Preview
 @Composable
 fun PreviewGoldPointChip() {
-    PointChip(
-        point = 1300,
-        theme = PointChipTheme.GOLD
-    )
+    ThemeProvider {
+        PointChip(
+            point = 130000000,
+            theme = PointChipTheme.GOLD
+        )
+    }
 }
 
 @Preview
 @Composable
 fun PreviewEaglePointChip2() {
-    PointChip(
-        point = 1300,
-        theme = PointChipTheme.EAGLE
-    )
+    ThemeProvider {
+        PointChip(
+            point = 13,
+            theme = PointChipTheme.EAGLE
+        )
+    }
 }
--- feature/challenge/src/main/java/com/umc/challenge/screen/ShopScreen.kt
+++ feature/challenge/src/main/java/com/umc/challenge/screen/ShopScreen.kt
@@ -15,13 +15,17 @@ import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -32,37 +36,38 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.round
 import androidx.compose.ui.unit.sp
 import com.umc.challenge.R
 import com.umc.challenge.component.PointChip
 import com.umc.challenge.component.PointChipTheme
 import com.umc.challenge.component.PurchaseDialog
 import com.umc.challenge.component.PurchaseDialogProp
-import com.umc.challenge.component.TitledTopBar
-import com.umc.challenge.component.TitledTopBarMode
-import com.umc.challenge.component.TitledTopBarProp
 import com.umc.challenge.view.previewAccessorySet
-import com.umc.design.Primary300
-import com.umc.design.Secondary100
 import com.umc.design.character.Accessory
 import com.umc.design.character.AccessorySet
+import com.umc.design.character.BodyPart
 import com.umc.design.character.CharacterView
+import com.umc.design.component.CustomHeader
+import com.umc.design.theme.LocalColorTheme
+import com.umc.design.theme.ThemeProvider
 import com.umc.design.R as Res
 
 data class ShopItemItemProp(
     val accessory: Accessory,
     val cost: Int,
+    val isOwned: Boolean,
     val onClicked: () -> Unit,
 )
 
@@ -72,34 +77,98 @@ private val itemItemShape = RoundedCornerShape(16.dp)
 @Composable
 fun ShopScreen(
     point: Int,
+    selectedBodyPart: BodyPart?,
     equippedAccessorySet: AccessorySet,
     shopItemItemPropList: List<ShopItemItemProp>,
     purchaseDialogProp: PurchaseDialogProp?,
-    onMyItemsIconClicked: () -> Unit
+    onMyItemsIconClicked: () -> Unit,
+    onBodyPartSelected: (BodyPart?) -> Unit,
+    onBackButtonClicked: () -> Unit,
 ) {
     val density = LocalDensity.current
     val scrollState = rememberLazyGridState()
 
-    var topBarHeight by remember { mutableStateOf(0.dp) }
-    var chipMenuVerticalOffset by remember { mutableStateOf(0.dp) }
+    var screenWidth: Dp? by remember { mutableStateOf(null) }
+    var topBarHeight: Dp? by remember { mutableStateOf(null) }
     var characterViewWidth by remember { mutableStateOf(0.dp) }
 
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = Color.Secondary100)
+            .onGloballyPositioned {
+                screenWidth = with(density) { it.size.width.toDp() }
+            },
     ) {
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Image(
+                painter = painterResource(R.drawable.img_challenge_background),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
         // 내용
         Column(
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
-            Spacer(modifier = Modifier.height(chipMenuVerticalOffset + 32.dp))
+            // 상단바
+            Box(
+                modifier = Modifier.onGloballyPositioned {
+                    topBarHeight = with(density) { it.size.height.toDp() }
+                },
+            ) {
+                CustomHeader(
+                    showLogo = false,
+                    backgroundColor = Color(0xFFFFE6E1).copy(alpha = 0.5f),
+                    waveColor = LocalColorTheme.current.primary[300],
+                    headerTrailing = {
+                        Box(
+                            modifier = Modifier.padding(vertical = 18.dp, horizontal = 21.98.dp)
+                        ) {
+                            PointChip(
+                                point = point,
+                                theme = PointChipTheme.EAGLE
+                            )
+                        }
+                    },
+                    onBackButtonClicked = onBackButtonClicked,
+                )
+            }
+            // 상단 메뉴
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(12.dp, alignment = Alignment.End),
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .padding(end = 16.dp)
+                    .fillMaxWidth()
+            ) {
+                Box(
+                    modifier = Modifier.clickable(
+                        indication = null,
+                        interactionSource = null,
+                        onClick = onMyItemsIconClicked
+                    )
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_my_items),
+                        contentDescription = null,
+                        contentScale = ContentScale.Fit,
+                        modifier = Modifier.size(24.dp)
+                    )
+                }
+            }
             // 캐릭터
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(32.dp)
-                    .onGloballyPositioned { with(density) { characterViewWidth = it.size.width.toDp() } }
+                    .onGloballyPositioned {
+                        with(density) {
+                            characterViewWidth = it.size.width.toDp()
+                        }
+                    }
             ) {
                 CharacterView(
                     accessorySet = equippedAccessorySet,
@@ -122,14 +191,46 @@ fun ShopScreen(
                     .fillMaxWidth()
             ) {
                 Box(
-                    modifier = Modifier.padding(8.dp)
+                    modifier = Modifier.padding(vertical = 16.dp)
                 ) {
                     Image(
                         painter = painterResource(id = Res.drawable.ic_header_deco),
                         contentDescription = null,
-                        contentScale = ContentScale.Fit,
-                        modifier = Modifier.size(32.dp)
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.width(39.53.dp)
                     )
+                }
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 4.dp, horizontal = 29.dp),
+                ) {
+                    listOf(
+                        null to "전체",
+                        BodyPart.HEAD to "머리",
+                        BodyPart.EYE to "눈",
+                        BodyPart.TORSO to "몸",
+                    ).forEach { (bodyPart, label) ->
+                        val isSelected = bodyPart == selectedBodyPart
+
+                        Box(
+                            modifier = Modifier.clickable(
+                                indication = null,
+                                interactionSource = null,
+                                onClick = { onBodyPartSelected(bodyPart) },
+                            )
+                        ) {
+                            Text(
+                                text = label,
+                                color = if (isSelected) LocalColorTheme.current.primary[600] else LocalColorTheme.current.grey[400],
+                                fontWeight = if (isSelected) FontWeight.W800 else FontWeight.W700,
+                                fontSize = 16.sp,
+                                lineHeight = 20.sp,
+                                modifier = Modifier.padding(horizontal = 4.dp, vertical = 2.dp),
+                            )
+                        }
+                    }
                 }
                 Box(
                     modifier = Modifier.weight(1f)
@@ -186,51 +287,35 @@ fun ShopScreen(
                 }
                 Spacer(
                     modifier = Modifier.height(
-                        WindowInsets.systemBars.asPaddingValues().calculateBottomPadding()
+                        WindowInsets.systemBars
+                            .asPaddingValues()
+                            .calculateBottomPadding()
                     )
                 )
             }
         }
-        // 탑 바
-        TitledTopBar(
-            prop = TitledTopBarProp(
-                mode = TitledTopBarMode.SHOP,
-                onHeightChanged = { topBarHeight = it }
-            )
-        )
-        Column(
-            modifier = Modifier.onGloballyPositioned {
-                with(density) { chipMenuVerticalOffset = it.size.height.toDp() }
-            }
-        ) {
-            Spacer(modifier = Modifier.height(topBarHeight))
-            // 상단 메뉴
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(12.dp, alignment = Alignment.End),
-                verticalAlignment = Alignment.CenterVertically,
+        // 상점 팻말 리소스
+        run {
+            val screenWidth = screenWidth
+            val topBarHeight = topBarHeight
+
+            if (screenWidth == null || topBarHeight == null) return@run
+
+            Image(
+                painter = painterResource(R.raw.img_shop_tag),
+                contentDescription = null,
+                contentScale = ContentScale.FillWidth,
                 modifier = Modifier
-                    .padding(end = 16.dp, top = 32.dp)
-                    .fillMaxWidth()
-            ) {
-                Box(
-                    modifier = Modifier.clickable(
-                        indication = null,
-                        interactionSource = null,
-                        onClick = onMyItemsIconClicked
-                    )
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.ic_my_items),
-                        contentDescription = null,
-                        contentScale = ContentScale.Fit,
-                        modifier = Modifier.size(24.dp)
-                    )
-                }
-                PointChip(
-                    point = point,
-                    theme = PointChipTheme.EAGLE
-                )
-            }
+                    .width(92.dp)
+                    .offset {
+                        with(density) {
+                            Offset(
+                                x = (screenWidth.toPx() - 92.dp.toPx()) / 2,
+                                y = topBarHeight.toPx() - 24.dp.toPx(),
+                            )
+                        }.round()
+                    }
+            )
         }
     }
 
@@ -244,73 +329,86 @@ private fun ShopItemItem(
     val density = LocalDensity.current
     var width by remember { mutableStateOf(0.dp) }
 
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(width)
-            .background(
-                brush = Brush.verticalGradient(
-                    0f to Color(0x80FEF6F2),
-                    1f to Color(0x80FFEAE2)
-                ),
-                shape = itemItemShape
-            )
-            .clip(itemItemShape)
-            .clickable { prop.onClicked() }
-            .border(
-                width = 1.dp,
-                color = Color(0xFFFFDACB),
-                shape = itemItemShape,
-            )
-            .onGloballyPositioned { with(density) { width = it.size.width.toDp() } }
+    Column(
+        verticalArrangement = Arrangement.spacedBy(10.dp)
     ) {
-        Row(
-            horizontalArrangement = Arrangement.End,
+        Box(
             modifier = Modifier
-                .padding(12.dp)
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .height(width)
+                .clip(itemItemShape)
+                .clickable { prop.onClicked() }
+                .border(
+                    width = 1.dp,
+                    color = LocalColorTheme.current.primary[200],
+                    shape = itemItemShape,
+                )
+                .onGloballyPositioned { with(density) { width = it.size.width.toDp() } }
         ) {
-            Box(
+            if (prop.isOwned) Row(
+                horizontalArrangement = Arrangement.End,
                 modifier = Modifier
-                    .background(
-                        shape = RoundedCornerShape(percent = 50),
-                        color = Color(0xFFFFEAE2)
-                    )
-                    .padding(horizontal = 8.dp, vertical = 2.dp)
+                    .padding(12.dp)
+                    .fillMaxWidth(),
             ) {
-                Text(
-                    text = "${prop.cost} ${stringResource(id = R.string.point_unit)}",
-                    fontWeight = FontWeight.W600,
-                    fontSize = 12.sp,
-                    color = Color.Primary300,
+                Box(
+                    modifier = Modifier
+                        .background(
+                            shape = RoundedCornerShape(percent = 50),
+                            color = LocalColorTheme.current.primary[100]
+                        )
+                        .padding(horizontal = 10.dp, vertical = 1.dp)
+                ) {
+                    Text(
+                        text = "보유",
+                        fontWeight = FontWeight.W700,
+                        fontSize = 13.sp,
+                        lineHeight = 20.sp,
+                        color = LocalColorTheme.current.primary[500],
+                    )
+                }
+            }
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Image(
+                    painter = painterResource(id = prop.accessory.shopRes),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxSize()
                 )
             }
         }
-        Box(
-            contentAlignment = Alignment.Center,
-            modifier = Modifier
-                .padding(32.dp)
-                .fillMaxSize()
-        ) {
-            Image(
-                painter = painterResource(id = prop.accessory.res),
-                contentDescription = null,
-                contentScale = ContentScale.Fit,
-                modifier = Modifier.fillMaxSize()
-            )
-        }
-        Box(
-            contentAlignment = Alignment.BottomCenter,
-            modifier = Modifier
-                .padding(bottom = 16.dp)
-                .fillMaxSize()
+        Column(
+            verticalArrangement = Arrangement.spacedBy(4.dp)
         ) {
             Text(
                 text = prop.accessory.title,
-                fontWeight = FontWeight.W600,
-                fontSize = 12.sp,
-                color = Color(0xFF4B4B4B),
+                fontWeight = FontWeight.W700,
+                fontSize = 15.sp,
+                lineHeight = 20.sp,
+                color = LocalColorTheme.current.grey[700],
+                modifier = Modifier.padding(start = 5.dp)
             )
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(5.36.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(start = 8.dp),
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.img_point_icon_1),
+                    contentDescription = null,
+                    contentScale = ContentScale.FillHeight,
+                    modifier = Modifier.height(18.dp),
+                )
+                Text(
+                    text = prop.cost.toString(),
+                    fontWeight = FontWeight.W700,
+                    fontSize = 14.sp,
+                    color = LocalColorTheme.current.grey[700],
+                )
+            }
         }
     }
 }
@@ -319,26 +417,31 @@ val previewShopItemItemPropList = listOf(
     ShopItemItemProp(
         accessory = Accessory.TTUTTU_PERL_NECKLACE,
         cost = 400,
+        isOwned = false,
         onClicked = {}
     ),
     ShopItemItemProp(
         accessory = Accessory.TTOTTO_COZY_MUFFLER,
         cost = 600,
+        isOwned = true,
         onClicked = {}
     ),
     ShopItemItemProp(
         accessory = Accessory.TTOTTO_CAP,
         cost = 300,
+        isOwned = false,
         onClicked = {}
     ),
     ShopItemItemProp(
         accessory = Accessory.TTUTTU_BAG,
         cost = 200,
+        isOwned = false,
         onClicked = {}
     ),
     ShopItemItemProp(
-        accessory = Accessory.TTUTTU_THREE_COLOR_BALLOONS,
+        accessory = Accessory.TTUTTU_GIANT_RIBBON,
         cost = 300,
+        isOwned = false,
         onClicked = {}
     )
 )
@@ -346,11 +449,16 @@ val previewShopItemItemPropList = listOf(
 @Preview
 @Composable
 fun PreviewShopScreen() {
-    ShopScreen(
-        point = 1234,
-        equippedAccessorySet = previewAccessorySet,
-        shopItemItemPropList = previewShopItemItemPropList,
-        purchaseDialogProp = null,
-        onMyItemsIconClicked = {}
-    )
+    ThemeProvider {
+        ShopScreen(
+            point = 1234,
+            selectedBodyPart = null,
+            equippedAccessorySet = previewAccessorySet,
+            shopItemItemPropList = previewShopItemItemPropList,
+            purchaseDialogProp = null,
+            onMyItemsIconClicked = {},
+            onBodyPartSelected = {},
+            onBackButtonClicked = {},
+        )
+    }
 }
