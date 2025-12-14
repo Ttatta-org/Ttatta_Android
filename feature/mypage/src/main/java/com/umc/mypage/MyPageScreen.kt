cf82446ee840222db6f08b1f77fb3715bb3f1915
CHOYUNSIG <cho.yunsig56@gmail.com>
2025-12-14 20:22:06 +0900

    :lipstick: 그림자 개선

--- feature/mypage/src/main/java/com/umc/mypage/MyPageScreen.kt
+++ feature/mypage/src/main/java/com/umc/mypage/MyPageScreen.kt
@@ -1,15 +1,24 @@
 package com.umc.mypage
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
@@ -19,30 +28,20 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.draw.dropShadow
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.shadow.Shadow
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil3.compose.AsyncImage
-import com.umc.core.model.LoginType
-import com.umc.core.model.UserInfo
-import com.umc.core.model.UserStatus
-import com.umc.mypage.components.BottomNavigationBarWithFAB
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.umc.core.model.UserInfo
 import com.umc.design.component.CustomHeader
 import com.umc.design.component.CustomPopup
-import com.umc.mypage.R
-import com.umc.mypage.components.Dialog
-import com.umc.mypage.components.TopBar_Mypage_Default
 import java.text.NumberFormat
 import java.util.Locale
 
@@ -195,21 +194,22 @@ fun SummarySection(
     diaryCount: Int,
     points: Long
 ) {
-
-    Card(
+    Box(
         modifier = Modifier
-            .fillMaxWidth() // ✅ 남은 공간을 가득 채우도록 설정
-            .shadow(
-                elevation = 2.dp, // 그림자의 높이 조정
-                shape = RoundedCornerShape(22.dp), // 카드의 모서리 둥글기
-                spotColor = Color(0xDE806E33),
-                ambientColor = Color(0xDE806E33),
-                clip = true // 모서리가 잘리도록 설정
-            ),
-        shape = RoundedCornerShape(22.dp),
-        colors = CardDefaults.cardColors(
-            containerColor = Color.White
-        )
+            .fillMaxWidth()
+            .dropShadow(
+                shape = RoundedCornerShape(22.dp),
+                shadow = Shadow(
+                    radius = 10.dp,
+                    color = Color(0xFF806E33),
+                    offset = DpOffset(0.dp, 2.dp),
+                    alpha = 0.1f,
+                ),
+            )
+            .background(
+                color = Color.White,
+                shape = RoundedCornerShape(22.dp),
+            )
     ) {
         Row(
             modifier = Modifier
@@ -278,22 +278,22 @@ fun AppSettingsSection(
     setShowLogoutDialog: (Boolean) -> Unit,
     onLogout: () -> Unit // ✅ 로그아웃 이벤트 추가
 ) {
-    Card(
+    Box(
         modifier = Modifier
-            //.width(340.dp) // 카드 너비 설정
-            .wrapContentHeight() // 카드 높이 동적으로 설정
-            .shadow(
-                elevation = 2.dp, // 그림자의 높이 조정
-                shape = RoundedCornerShape(22.dp), // 카드의 모서리 둥글기
-                spotColor = Color(0xDE806E33),
-                ambientColor = Color(0xDE806E33),
-                clip = true // 모서리가 잘리도록 설정
-            ),
-        shape = RoundedCornerShape(22.dp),
-        elevation = CardDefaults.cardElevation(2.dp),
-        colors = CardDefaults.cardColors(
-            containerColor = Color.White // 카드 배경 색상 설정
-        )
+            .fillMaxWidth()
+            .dropShadow(
+                shape = RoundedCornerShape(22.dp),
+                shadow = Shadow(
+                    radius = 10.dp,
+                    color = Color(0xFF806E33),
+                    offset = DpOffset(0.dp, 2.dp),
+                    alpha = 0.1f,
+                ),
+            )
+            .background(
+                color = Color.White,
+                shape = RoundedCornerShape(22.dp),
+            )
     ) {
         Box {
             Column(
@@ -391,22 +391,22 @@ fun AppSettingsSection(
 
     Spacer(modifier = Modifier.height(12.dp))
 
-    Card(
+    Box(
         modifier = Modifier
-            //.width(340.dp) // 카드 너비 설정
-            .wrapContentHeight() // 카드 높이 동적으로 설정
-            .shadow(
-                elevation = 2.dp, // 그림자의 높이 조정
-                shape = RoundedCornerShape(22.dp), // 카드의 모서리 둥글기
-                spotColor = Color(0xDE806E33),
-                ambientColor = Color(0xDE806E33),
-                clip = true // 모서리가 잘리도록 설정
-            ),
-        shape = RoundedCornerShape(22.dp),
-        elevation = CardDefaults.cardElevation(2.dp),
-        colors = CardDefaults.cardColors(
-            containerColor = Color.White // 카드 배경 색상 설정
-        )
+            .fillMaxWidth()
+            .dropShadow(
+                shape = RoundedCornerShape(22.dp),
+                shadow = Shadow(
+                    radius = 10.dp,
+                    color = Color(0xFF806E33),
+                    offset = DpOffset(0.dp, 2.dp),
+                    alpha = 0.1f,
+                ),
+            )
+            .background(
+                color = Color.White,
+                shape = RoundedCornerShape(22.dp),
+            )
     ) {
         Box {
             Column(
